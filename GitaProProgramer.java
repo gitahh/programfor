14d06b96ec00d302eea570af95bf54459422a627
gitahh <39770822+gitahh@users.noreply.github.com>
2018-05-30 21:40:55 +0700

    Add files via upload

---
+++ GitaProProgramer.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitaproprogramer;
+
+/**
+ *
+ * @author HP
+ */
+public class GitaProProgramer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for(int i=0; i<11; i++)
+            System.out.println(i + " * 8 = " + (i*8));
+    }
+}
